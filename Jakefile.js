3d2acd355203367cd4c1fd22942fc1daf404f669
ddrdushy <dushy2009@gmail.com>
2016-07-30 13:48:18 +0530

    node version is added

--- Jakefile.js
+++ Jakefile.js
@@ -5,9 +5,23 @@
 (function(){
     "use strict";
 
+
     desc("Default Task");
-    task("default",function(){
+    task("default",["Version"],function(){
         console.log("\n\nBuild OK!!");
+    });
+
+    desc("version check");
+    task("Version",function(){
+        console.log("Checking Version");
+
+        var packageJson=require("./package.json");
+        var expected_version="v4.4.3";
+        var actual_version=packageJson.engines.node;
+
+        if(actual_version!==expected_version)
+            fail("Incorrect Node version. Expected "+expected_version+ "but Actual "+actual_version);
+        console.log(process.version);
     });
 
 }());
