4b4f6dcb4bf9679e76b65dd0c38f92fff20c790d
ddrdushy <dushy2009@gmail.com>
2016-09-23 12:18:13 +0530

    user class issue,, constructor not wotking

--- eod.php
+++ eod.php
@@ -1,8 +1,8 @@
-<?php include("userclass.php"); ?>
-
 <?php
+include("userclass.php");
 // Parse without sections
 $ini_array = parse_ini_file("configure.ini");
+
 echo $ini_array["CAMPSITE_ID"]."\n";
 
 $result=file_get_contents("https://api.gitter.im/v1/rooms?access_token=".$ini_array["API_KEY"]);
@@ -11,16 +11,25 @@ $user_count=$object[3]->userCount;
 echo $object[3]->userCount."\n";
 
 $user_list=array();
+$newuserarray=array();
 
-  for($x=0;$x<$user_count;$x+=30){
+
+  for($x=0;$x<5;$x+=30){
       $result=file_get_contents("https://api.gitter.im/v1/rooms/".$ini_array["CAMPSITE_ID"]."/users?access_token=".$ini_array["API_KEY"]."&skip=".$x);
       echo $x."\n";
       $user_list[]=json_decode($result);
   }
 
-  
-  $user_list_count=count($user_list[0]);
+  for($x=0;$x<count($user_list);$x++){
+    echo "x: ".$x."\n";
+    for($y=0;$y<count($user_list[$x]);$y++){
+      echo $user_list[$x][$y]->username."\n";
+      $newuserarray[]=new user($user_list[$x][$y]->id,$user_list[$x][$y]->username,$user_list[$x][$y]->avatarUrlMedium,$user_list[$x][$y]->displayName);
+    }
+  }
 
-  echo $user_list_count;
+  $user_list_count=count($newuserarray);
+  print_r ($newuserarray);
+  //echo $newuserarray[0]->name;
 
 ?>
--- userclass.php
+++ userclass.php
@@ -2,19 +2,23 @@
   class user{
     var $id;
     var $uname;
+    var $name;
     var $img;
     var $fccurl;
     var $apiurl;
     var $points;
     var $rank;
 
-    function _construct($id,$uname,$img){
+    function _construct($id,$uname,$img,$name){
+      echo "i am in\n";
       $this->id=$id;
       $this->uname=$uname;
+      $this->name=$name;
       $this->img=$img;
       $this->fccurl="https://www.freecodecamp.com/".$uname;
       $this->apiurl="https://www.freecodecamp.com/api/users/about?username=".$uname;
       $this->points=$this->pointsFetcher();
+      echo "i am out\n";
     }
 
     function pointsFetcher(){
