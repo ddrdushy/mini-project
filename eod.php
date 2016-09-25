ccb660d89cfd682e2dd755138d2e4ebaa8498fc5
ddrdushy <dushy2009@gmail.com>
2016-09-25 14:28:41 +0530

    user update function is created

--- eod.php
+++ eod.php
@@ -1,7 +1,7 @@
 <?php
 include("userclass.php");
 // Parse without sections
-$ini_array = parse_ini_file("configure.ini");
+$ini_array= parse_ini_file("configure.ini");
 //echo $ini_array["CAMPSITE_ID"]."\n";
 
   //get the user count from gitter room
@@ -23,9 +23,8 @@ $ini_array = parse_ini_file("configure.ini");
    }
   print_r($dbcount);
 
-  if($user_count!=$dbcount){
-    userUpdate();
-  }
+  userUpdate($user_count);
+
 //user updation in table is finished
 
 //select the users from user table and add them to the user class
@@ -58,14 +57,58 @@ $ini_array = parse_ini_file("configure.ini");
    //data insertion to the table
    for($i=0;$i<count($user_list);$i++){
      $total_points += $user_list[$i]->points;
-     //$qry="INSERT INTO `daily_update`(`r_date`, `uid`, `points`, `rank`) VALUES ('". date("Y-m-d") ."','".$user_list[$i]->id."',".$user_list[$i]->points.",".($i+1).")";
+     $qry="INSERT INTO `daily_update`(`r_date`, `uid`, `points`, `rank`) VALUES ('". date("Y-m-d") ."','".$user_list[$i]->id."',".$user_list[$i]->points.",".($i+1).")";
      //echo $qry."\n";
-     //$res=mysqli_query($link,$qry) or die (mysqli_error($link));
+     $res=mysqli_query($link,$qry) or die (mysqli_error($link));
      //echo $res."\n";
    }
+
+   //daily update table is updated
    $qry="INSERT INTO `daily_count`(`u_date`, `pts_count`, `u_count`) VALUES ('". date("Y-m-d") ."',". $total_points.",".count($user_list).")";
    $res=mysqli_query($link,$qry) or die (mysqli_error($link));
    echo $total_points;
+
+   //userUpdate function()
+  function userUpdate($user_count){
+
+        $ini_array= parse_ini_file("configure.ini");
+
+        $link = mysqli_connect('localhost','root','','mini');
+          if (!$link) {
+            die('Could not connect to MySQL: ' . mysql_error());
+          }
+
+        $user_list_new=array();
+
+        //array for user list
+           for($x=0;$x<$user_count;$x+=30){
+               $result=file_get_contents("https://api.gitter.im/v1/rooms/".$ini_array["CAMPSITE_ID"]."/users?access_token=".$ini_array["API_KEY"]."&skip=".$x);
+               echo $x."\n";
+               $user_list_new[]=json_decode($result);
+           }
+
+         //user updation and insertion
+         for($x=0;$x<count($user_list_new);$x++){
+             for($y=0;$y<count($user_list_new[$x]);$y++){
+
+               $qry="SELECT * FROM `user` WHERE `uid`='".$user_list_new[$x][$y]->id."'";
+               $res=mysqli_query($link,$qry) or die (mysqli_error($link));
+
+                   if(!$res){
+                     $qry="INSERT INTO `user`(`uid`, `name`, `doj`, `uname`, `url`) VALUES ('".$user_list_new[$x][$y]->id."','".$user_list_new[$x][$y]->displayName."','". date("Y-m-d") ."','".$user_list_new[$x][$y]->username."','".$user_list_new[$x][$y]->avatarUrlMedium."')";
+                     echo $qry."\n";
+                   }else{
+                     $qry ="UPDATE `user` SET `url`='".$user_list_new[$x][$y]->avatarUrlMedium."' WHERE `uid`='".$user_list_new[$x][$y]->id."'";
+                     echo $qry."\n";
+                   }
+
+               $res=mysqli_query($link,$qry) or die (mysqli_error($link));
+               echo $res."\n";
+             }
+         }
+  }
+
+
 /*
 
 $newuserarray=array();
