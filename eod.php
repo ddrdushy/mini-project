1f323336d639f5336f83cac3ff28772b8b429047
ddrdushy <dushy2009@gmail.com>
2016-09-25 11:55:09 +0530

    based on the to do list the file is updated

--- eod.php
+++ eod.php
@@ -2,16 +2,53 @@
 include("userclass.php");
 // Parse without sections
 $ini_array = parse_ini_file("configure.ini");
+//echo $ini_array["CAMPSITE_ID"]."\n";
 
-echo $ini_array["CAMPSITE_ID"]."\n";
+  //get the user count from gitter room
+  $result=file_get_contents("https://api.gitter.im/v1/rooms?access_token=".$ini_array["API_KEY"]);
+  $object = json_decode($result);
+  $user_count=$object[3]->userCount;
+  echo $object[3]->userCount."\n";
+  //got the user count in $user_count variable
 
-$result=file_get_contents("https://api.gitter.im/v1/rooms?access_token=".$ini_array["API_KEY"]);
-$object = json_decode($result);
-$user_count=$object[3]->userCount;
-echo $object[3]->userCount."\n";
+  //get the user count from table
+  $link = mysqli_connect('localhost','root','','mini');
+  if (!$link) {
+    die('Could not connect to MySQL: ' . mysql_error());
+  }
+  $qry="SELECT count(*) FROM `user`";
+  $res=mysqli_query($link,$qry) or die (mysqli_error($link));
+  while($row = mysqli_fetch_array($res, MYSQL_ASSOC)) {
+      $dbcount=$row["count(*)"];
+   }
+  print_r($dbcount);
 
-$user_list=array();
+  if($user_count!=$dbcount){
+    userUpdate();
+  }
+//user updation in table is finished
+
+//select the users from user table and add them to the user class
+  $qry="SELECT * FROM `user` WHERE `excluder`='N'";
+  $res=mysqli_query($link,$qry) or die (mysqli_error($link));
+  //array for user list
+  $user_list=array();
+
+  //insert the fetched data to the array
+  while($row = mysqli_fetch_array($res, MYSQL_ASSOC)) {
+    $user_list[]=new user($row->uid,$row->uname,$row->name);
+   }
+
+
+
+/*
+
 $newuserarray=array();
+
+
+
+$qry="SELECT * FROM `user` WHERE `excluder`='N' ";
+
 
 
   for($x=0;$x<$user_count;$x+=30){
@@ -40,8 +77,17 @@ $newuserarray=array();
   }
 
   usort($newuserarray,"cmp");
-  print_r($newuserarray);
+  //print_r($newuserarray);
   echo "\n".$newuserarray[0]->points;
 
+
+  for($a=0;$a<count($newuserarray);$a++){
+  //  $qry="INSERT INTO `user`(`uid`, `name`, `doj`, `uname`, `url`) VALUES ('".$newuserarray[$a]->id."','".$newuserarray[$a]->name."','". date("Y-m-d") ."','".$newuserarray[$a]->uname."','".$newuserarray[$a]->img."')";
+    //$res=mysqli_query($link,$qry) or die (mysqli_error($link));
+
+    echo $res;
+  }
+
+*/
 
 ?>
