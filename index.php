3e34a9b078b19f34c19cefee4812be1b70ede279
Administrator <admin@example.com>
2016-09-29 15:40:45 +0530

    index page is aligned

--- index.php
+++ index.php
@@ -67,107 +67,75 @@ if (!$link) {
 
     <!--carousel start -->
     <div class="container-fluid">
-    <div id="myCarousel" class="carousel slide" data-ride="carousel">
-      <!-- Indicators -->
-      <!--
-      <ol class="carousel-indicators">
-        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-        <li data-target="#myCarousel" data-slide-to="1"></li>
-        <li data-target="#myCarousel" data-slide-to="2"></li>
-        <li data-target="#myCarousel" data-slide-to="3"></li>
-      </ol>
-      -->
-      <!-- Wrapper for slides -->
-      <div class="carousel-inner" role="listbox">
-      <?php
-      $qry="SELECT * FROM `daily_count` where `u_date`='".date('Y-m-d',strtotime("-1 days"))."'";
-      //echo $qry;
-      $qry2=mysqli_query($link,$qry) or die (mysqli_error($link));
-      while($row = mysqli_fetch_array($qry2, MYSQL_ASSOC)) {
-            $pts=$row['pts_count'];
-            $ucount=$row['u_count'];
-      }
-          echo "<div class=\"item active\">
-                    <div class=\"cont\">
-                    <div class=\"row\">
-                        <div class=\"col-lg-12 text-center\">
-                          <h1>Total Problems:".$pts." </h1>
-                        </div>
-                    </div>
-                    <div class=\"row\">
-                        <div class=\"col-lg-12 text-center\">
-                          <h1>Total Problems:".$ucount." </h1>
-                        </div>
-                    </div>
-                    </div>
-                </div>";
+      <div id="myCarousel" class="carousel slide" data-ride="carousel">
+            <!-- Wrapper for slides -->
+            <div class="carousel-inner" role="listbox">
 
-      $qry="SELECT `user`.`name`,`user`.`url`,`daily_update`.`points` FROM `daily_update`,`user` WHERE `user`.`uid`=`daily_update`.`uid` and `daily_update`.`r_date`='".date('Y-m-d',strtotime("-1 days"))."' ORDER BY `daily_update`.`points` DESC limit 3";
-      //echo $qry;
-      $qry2=mysqli_query($link,$qry) or die (mysqli_error($link));
-      $count=0;
-       while($row = mysqli_fetch_array($qry2, MYSQL_ASSOC)) {
-             $count++;
-             $name=$row['name'];
-             $url=$row['url'];
-             $points=$row['points'];
-             echo "<div class=\"item\">
-             <h1 class=\"col-lg-12 text-center\"><span class=\"label label-primary\">Maximum Problem Solver</span></h1>
-                 <div class=\"row\">
-                     <div class=\"col-lg-6 count\">
-                       <h2>#".$count."</h2>
-                     </div>
-                     <div class=\"col-lg-6\">
-                       <div class=\"cont\">
-                         <h3>".$name."</h3>
-                         <h4>".$points."</h4>
-                         <img src=\"".$url."\"></img>
-                      </div>
-                     </div>
-                 </div>
-               </div>\n";
-         }
-      ?>
-        </div>
-      </div>
-<!--
-        <div class="item">
-          <div class="row">
-              <div class="col-lg-6">
-                <img src="http://rootear.com/files/2014/09/logo-prin-java.jpg"></img>
-              </div>
-              <div class="col-lg-6">
-                <h1><span class="label label-info">Maximum Problem Solver</span></h1>
-                <h2>#2</h2>
-              </div>
-          </div>
-        </div>
+                <?php
+                    $qry="SELECT * FROM `daily_count` where `u_date`='".date('Y-m-d',strtotime("-1 days"))."'";
+                    //echo $qry;
+                    $qry2=mysqli_query($link,$qry) or die (mysqli_error($link));
 
-        <div class="item">
-          <div class="row">
-              <div class="col-lg-6">
-                <img src="http://rootear.com/files/2014/09/logo-prin-java.jpg"></img>
-              </div>
-              <div class="col-lg-6">
-                <h1><span class="label label-success">Maximum Problem Solver</span></h1>
-                <h2>#3</h2>
-              </div>
-          </div>
-        </div>
--->
+                        while($row = mysqli_fetch_array($qry2, MYSQL_ASSOC)) {
+                              $pts=$row['pts_count'];
+                              $ucount=$row['u_count'];
+                        }
 
+                        echo "<div class=\"item active\">
+                                  <div class=\"cont\">
+                                  <div class=\"row\">
+                                      <div class=\"col-lg-12 text-center\">
+                                        <h1>Total Problems:".$pts." </h1>
+                                      </div>
+                                  </div>
+                                  <div class=\"row\">
+                                      <div class=\"col-lg-12 text-center\">
+                                        <h1>Total Problems:".$ucount." </h1>
+                                      </div>
+                                  </div>
+                                  </div>
+                              </div>";
 
-      <!-- Left and right controls -->
-      <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-      </a>
-      <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-      </a>
-      </div>
-    <!--carousel end -->
+                    $qry="SELECT `user`.`name`,`user`.`url`,`daily_update`.`points` FROM `daily_update`,`user` WHERE `user`.`uid`=`daily_update`.`uid` and `daily_update`.`r_date`='".date('Y-m-d',strtotime("-1 days"))."' ORDER BY `daily_update`.`points` DESC limit 3";
+                    //echo $qry;
+                    $qry2=mysqli_query($link,$qry) or die (mysqli_error($link));
+                    $count=0;
+
+                        while($row = mysqli_fetch_array($qry2, MYSQL_ASSOC)) {
+                           $count++;
+                           $name=$row['name'];
+                           $url=$row['url'];
+                           $points=$row['points'];
+                           echo "<div class=\"item\">
+                           <h1 class=\"col-lg-12 text-center\"><span class=\"label label-primary\">Maximum Problem Solver</span></h1>
+                               <div class=\"row\">
+                                   <div class=\"col-lg-6 count\">
+                                     <h2>#".$count."</h2>
+                                   </div>
+                                   <div class=\"col-lg-6\">
+                                     <div class=\"cont\">
+                                       <h3>".$name."</h3>
+                                       <h4>".$points."</h4>
+                                       <img src=\"".$url."\"></img>
+                                    </div>
+                                   </div>
+                               </div>
+                             </div>\n";
+                       }
+                ?>
+
+                <!-- Left and right controls -->
+                <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+                  <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                  <span class="sr-only">Previous</span>
+                </a>
+                <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+                  <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                  <span class="sr-only">Next</span>
+                </a>
+            </div>
+          <!--carousel end -->
+    </div>
 
   </body>
 </html>
