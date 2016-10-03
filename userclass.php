dfc1d28d4f08609ac676289d0234da9736203e6b
Administrator <admin@example.com>
2016-10-03 09:46:34 +0530

    userclass.php updated with curl

--- userclass.php
+++ userclass.php
@@ -14,15 +14,33 @@
       $this->name=$name;
       $this->fccurl="https://www.freecodecamp.com/".$uname;
       $this->apiurl="https://www.freecodecamp.com/api/users/about?username=".strtolower($uname);
-      $this->points = self::pointsFetcher();
+      $this->points ;//= self::pointsFetcher();
       echo "i am out\n";
     }
 
     function pointsFetcher(){
       try{
-      $result=file_get_contents($this->apiurl);
+      /*$result=file_get_contents($this->apiurl);
       $object = json_decode($result);
-      echo "got the data\n";
+      echo "got the data\n";*/
+
+
+      //  Initiate curl
+      $ch = curl_init();
+      // Disable SSL verification
+      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+      // Will return the response, if false it print the response
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+      // Set the url
+      curl_setopt($ch, CURLOPT_URL,$this->apiurl);
+      // Execute
+      $object=curl_exec($ch);
+      // Closing
+      curl_close($ch);
+
+      // Will dump a beauty json :3
+      var_dump(json_decode($object, true));
+
       if(property_exists($object, "about"))
          return empty($object->about->browniePoints) ? 0 : $object->about->browniePoints;
       else
