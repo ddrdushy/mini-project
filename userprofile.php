45e11140b6afdd644b1ea0aade37897d0e7d4f70
ddrdushy <dushy2009@gmail.com>
2016-10-06 00:03:33 +0530

    users are listed and redirected to the profile page

--- userprofile.php
+++ userprofile.php
@@ -4,6 +4,30 @@
       <link rel="stylesheet" type="text/css" href="node_modules\bootstrap\dist\css\animate.css">
       <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
       <script src="node_modules\bootstrap\dist\js\bootstrap.js"></script>
+      <style>
+        .img{
+          position:absolute;
+        }
+        h2,h3{
+          padding-left: 85px;
+          padding-top: -5px;
+        }
+        .img{
+          padding-top: 30px;
+        }
+        body{
+          background-image:url("img/bg2.jpg");
+          color: #fff;
+          background-repeat: no-repeat;
+          background-attachment: fixed;
+        }
+        a:link{
+          color: #fff;
+        }
+        .table-hover tbody tr:hover td, .table-hover tbody tr:hover th {
+          background-color: #000;
+        }
+      </style>
     </head>
 
     <body>
@@ -26,7 +50,39 @@
       </nav>
       <!--navigation menu end-->
 
+      <div class="container-fluid">
+          <table class="table table-hover">
+            <?php
+                $link = mysqli_connect('localhost','root','','mini');
+                if (!$link) {
+                  die('Could not connect to MySQL: ' . mysql_error());
+                }
 
+                $qry="select `user`.`name`,`user`.`uname`,`user`.`url`,`user`.`uid`,`daily_update`.`points` from `user`,`daily_update` where `user`.`uid`=`daily_update`.`uid` and `daily_update`.`r_date`='".date("Y-m-d")."' and `user`.`excluder`='N' ORDER BY `daily_update`.`points` DESC";
+                $res=mysqli_query($link,$qry) or die (mysqli_error($link));
+                $count=0;
+                $str="<tr>";
+                while($row = mysqli_fetch_array($res, MYSQL_ASSOC)) {
+                  $count++;
+                  $str=$str."<td class=\"col-lg-4\">
+                             <a href=\"profile.php?uid=".$row["uid"]."\"><div class=\"img\" >
+                                  <img src=".$row["url"]." width=\"75px\" height=\"75px\" />
+                             </div>
+                             <span><h2>".$row["name"]."</h2></span>
+                             <span><h3>".$row["points"]."</h3></span></a>
+                           </td>";
+
+                 if($count%3==0)
+                   $str=$str."</tr><tr>";
+                  echo $str;
+                  $str="";
+
+                }
+
+             ?>
+
+          </table>
+      </div>
 
 
 
