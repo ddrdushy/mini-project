7c7c08926b3dcad5a1d08ac4383f2c8cac8ef09e
ddrdushy <dushy2009@gmail.com>
2016-10-05 08:11:02 +0530

    multiresponse test file

--- test.php
+++ test.php
@@ -1,60 +1,63 @@
-<?php 
+<?php
 	function multiRequest($data, $options = array()) {
-	 
+
 	  // array of curl handles
 	  $curly = array();
 	  // data to be returned
 	  $result = array();
-	 
+
 	  // multi handle
 	  $mh = curl_multi_init();
-	 
+
 	  // loop through $data and create curl handles
 	  // then add them to the multi-handle
 	  foreach ($data as $id => $d) {
-	 
+
 	    $curly[$id] = curl_init();
-	 
+
 	    $url = (is_array($d) && !empty($d['url'])) ? $d['url'] : $d;
-	    curl_setopt($curly[$id], CURLOPT_URL,            $url);
-	    curl_setopt($curly[$id], CURLOPT_HEADER,         0);
+			echo $url."\n";
+	    curl_setopt($curly[$id], CURLOPT_URL,$url);
+	    curl_setopt($curly[$id], CURLOPT_HEADER,0);
 	    curl_setopt($curly[$id], CURLOPT_RETURNTRANSFER, 1);
-	 
+			curl_setopt($curly[$id], CURLOPT_SSL_VERIFYPEER, false);
+			// Will return the response, if false it print the response
+			curl_setopt($curly[$id], CURLOPT_RETURNTRANSFER, true);
+
 	    // post?
 	    if (is_array($d)) {
 	      if (!empty($d['post'])) {
-		curl_setopt($curly[$id], CURLOPT_POST,       1);
-		curl_setopt($curly[$id], CURLOPT_POSTFIELDS, $d['post']);
+					curl_setopt($curly[$id], CURLOPT_POST,1);
+					curl_setopt($curly[$id], CURLOPT_POSTFIELDS, $d['post']);
 	      }
 	    }
-	 
+
 	    // extra options?
 	    if (!empty($options)) {
 	      curl_setopt_array($curly[$id], $options);
 	    }
-	 
 	    curl_multi_add_handle($mh, $curly[$id]);
 	  }
-	 
+
 	  // execute the handles
 	  $running = null;
 	  do {
 	    curl_multi_exec($mh, $running);
 	  } while($running > 0);
-	 
-	 
+
+
 	  // get content and remove handles
 	  foreach($curly as $id => $c) {
 	    $result[$id] = curl_multi_getcontent($c);
+			echo $c."\n";
 	    curl_multi_remove_handle($mh, $c);
 	  }
-	 
+
 	  // all done
 	  curl_multi_close($mh);
-	 
 	  return $result;
 	}
-	 
+
 
 
 $data = array(
@@ -84,7 +87,5 @@ $data = array(
   'https://www.freecodecamp.com/api/users/about?username=santhoshnv'
 );
 $r = multiRequest($data);
- 
-echo '<pre>';
 print_r($r);
 ?>
